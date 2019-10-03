6542b55bb87eb029e8c662e554aa2e93b8dc1ea4
Raphaël Pinel <raphaelpinel@gmail.com>
2019-10-03 13:15:04 +0300

    add final screen and restart button

--- lib/main.dart
+++ lib/main.dart
@@ -21,13 +21,17 @@ class _MyAppState extends State<MyApp> {
   ];
 
   _answerQuestion() {
-    if (_questionIndex < _questions.length - 1) {
-      setState(() {
-        _questionIndex = _questionIndex + 1;
-      });
-    }
+    setState(() {
+      _questionIndex = _questionIndex + 1;
+    });
     print(_questionIndex);
   }
+
+_restartQuizz() {
+  setState(() {
+    _questionIndex = 0;
+  });
+}
 
   @override
   Widget build(BuildContext context) {
@@ -59,30 +63,57 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(
           title: Text('Personality Quizz'),
         ),
-        body: Column(
-          children: <Widget>[
-            Question(_questions[_questionIndex]['questionText']),
-            Container(
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text.rich(
-                  TextSpan(text: 'Hello', children: [
-                    TextSpan(
-                        text: ' beautiful ',
-                        style: TextStyle(fontStyle: FontStyle.italic)),
-                    TextSpan(
-                        text: 'world',
-                        style: TextStyle(fontWeight: FontWeight.bold)),
-                  ]),
-                  style: TextStyle(fontSize: 20),
+        body: _questionIndex < _questions.length
+            ? Column(
+                children: <Widget>[
+                  Question(_questions[_questionIndex]['questionText']),
+                  Container(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text.rich(
+                        TextSpan(text: 'Hello', children: [
+                          TextSpan(
+                              text: ' beautiful ',
+                              style: TextStyle(fontStyle: FontStyle.italic)),
+                          TextSpan(
+                              text: 'world',
+                              style: TextStyle(fontWeight: FontWeight.bold)),
+                        ]),
+                        style: TextStyle(fontSize: 20),
+                      ),
+                    ),
+                  ),
+                  ...(_questions[_questionIndex]['answers'] as List<String>)
+                      .map((answer) => Answer(answer, _answerQuestion))
+                      .toList(),
+                ],
+              )
+            : Center(
+                child: Container(
+                  alignment: Alignment.center,
+                  color: Colors.orange,
+                  width: double.infinity,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text(
+                        'You did it!',
+                        style: TextStyle(fontSize: 36, color: Colors.white, fontWeight: FontWeight.bold),
+                      ),
+                      Container(
+                        margin: EdgeInsets.only(top: 20),
+                        child: RaisedButton(
+                          child: Text('Start again'),
+                          color: Colors.green,
+                          textColor: Colors.white,
+                          onPressed: _restartQuizz,
+                          
+                        ),
+                      )
+                    ],
+                  ),
                 ),
               ),
-            ),
-            ...(_questions[_questionIndex]['answers'] as List<String>)
-                .map((answer) => Answer(answer, _answerQuestion))
-                .toList(),
-          ],
-        ),
       ),
     );
   }
